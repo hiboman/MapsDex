1cfdeacec26b4401c4e0fd4390ed49f3319ae2ac
HiboMan <hiboman@proton.me>
2026-02-21 14:30:25 +0100

    Update local.py

--- admin_panel/admin_panel/settings/local.py
+++ admin_panel/admin_panel/settings/local.py
@@ -2,8 +2,8 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = "insecure"
-INSTALLED_APPS.append("achievement")
-INSTALLED_APPS.append("craftings")
+#INSTALLED_APPS.append("achievement")
+#INSTALLED_APPS.append("craftings")
 
 # Only allow connections from local IPs
 ALLOWED_CIDR_NETS = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
